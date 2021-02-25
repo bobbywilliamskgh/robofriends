4e1fbc734ce54145735ded7550cec0525d5c07d0
Bobby Williams <bobbywilliam86@gmail.com>
2021-02-25 19:13:16 +0700

    README changed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
+import CardList from "./CardList";
+import { robots } from "./robots";
+import SearchBox from "./SearchBox";
+import Scroll from "./Scroll";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+            searchField: "",
+        }
+        // console.log('constructor');
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchField: event.target.value });
+    }
+
+    componentDidMount() {
+        // console.log('componentDidMount');
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then((robots) => { this.setState({ robots: robots }) });
+
+    }
+
+    render() {
+        // console.log('render');
+        const filteredRobots = robots.filter((robot) => {
+            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+        });
+        if (this.state.robots.length == 0) {
+            return <h1>Loading...</h1>
+        } else {
+            return (
+                <div className="tc">
+                    <h1 className="f1">Robo Friends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll>
+                        <CardList robots={filteredRobots} />
+                    </Scroll>
+                  
+                </div>
+            );
+        }
+
+
+    }
 }
 
 export default App;
---
+++ src/Scroll.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Scroll.css';
+
+
+const Scroll = (props) => {
+    return (
+        <div className="container">
+            {props.children}
+        </div>
+    );
+}
+
+export default Scroll;
