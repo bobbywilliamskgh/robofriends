159bbc7fae53fe72008ba27208d48d979eda834d
Bobby Williams <bobbywilliam86@gmail.com>
2021-02-25 20:17:40 +0700

    change folder structur

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
-import CardList from "./CardList";
-import { robots } from "./robots";
-import SearchBox from "./SearchBox";
-import Scroll from "./Scroll";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
 import './App.css';
 
 
@@ -29,26 +28,23 @@ class App extends Component {
     }
 
     render() {
+        const { robots, searchField } = this.state;
         // console.log('render');
         const filteredRobots = robots.filter((robot) => {
-            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
         });
-        if (this.state.robots.length == 0) {
-            return <h1>Loading...</h1>
-        } else {
-            return (
+        return !robots.length ?
+            <h1>Loading...</h1> :
+            (
                 <div className="tc">
                     <h1 className="f1">Robo Friends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
                     <Scroll>
                         <CardList robots={filteredRobots} />
                     </Scroll>
-                  
+
                 </div>
             );
-        }
-
-
     }
 }
 
